ad44e33c886ae6be3f0115632d12705013412005
zjcrumpton <Zaccary.Crumpton@charter.com>
2023-01-20 01:15:30 -0700

    extra spacing

--- src/endpoints/journals/getJournals.ts
+++ src/endpoints/journals/getJournals.ts
@@ -4,7 +4,7 @@ const BASE_URL = 'https://api.crossref.org/';
 const ENDPOINT = 'journals/';
 
 // Return a list of journals in the Crossref database.
-export const getJournals = async  () => {
+export const getJournals = async () => {
   try {
     const url = BASE_URL + ENDPOINT;
     const result = await axios.get(url);
