cbb4100aefd6d6bfd04096e956bca64d1f433e59
Dmitri Ramos <odreastonbjj@gmail.com>
2023-01-21 17:42:46 -0700

    Added sort options list

--- src/types/RequestConfig.ts
+++ src/types/RequestConfig.ts
@@ -1,13 +1,21 @@
 export interface RequestConfig {
-    rows?: number, // The number of rows per page
-    offset?: number, // The number of rows to skip before returning
-    mailto?: string, // The email address to identify yourself and be in the “polite pool”
-    sort?: SortOptions[], // Results can be sorted by applying the sort and order parameters. sort sets the field by which results will be sorted. order sets the result ordering, either asc or desc (default is desc).
+  rows?: number; // The number of rows per page
+  offset?: number; // The number of rows to skip before returning
+  mailto?: string; // The email address to identify yourself and be in the “polite pool”
+  sort?: SortOptions[]; // Results can be sorted by applying the sort and order parameters. sort sets the field by which results will be sorted. order sets the result ordering, either asc or desc (default is desc).
 }
 
-// TODO - add all sort options
 export enum SortOptions {
-  CREATED = 'created',
-  DEPOSITED = 'deposited',
-  INDEXED = 'indexed',
+  CREATED = 'created', // sort by created date
+  DEPOSITED = 'deposited', // sort by time of most recent deposit
+  INDEXED = 'indexed', // sort by time of most recent index
+  IS_REFERENCED_BY_COUNT = 'is-referenced-by-count', // sort by number of times this DOI is referenced by other Crossref DOIs
+  ISSUED = 'issued', // sort by issued date (earliest known publication date)
+  PUBLISHED = 'published', // sort by publication date
+  PUBLISHED_ONLINE = 'published-online', // sort by online publication date
+  PUBLISHED_PRINT = 'published-print', // sort by print publication date
+  REFERENCES_COUNT = 'references-count', // sort by number of references included in the references section of the document identified by this DOI
+  RELEVANCE = 'relevance', // sort by relevance score
+  SCORE = 'score', // sort by relevance score
+  UPDATED = 'updated', // sort by date of most recent change to metadata, currently the same as deposited
 }
